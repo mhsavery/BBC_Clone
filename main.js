341e62597b130dc705d2e2b449cf225b6113f2b5
Matthew Savery <mhsavery126@gmail.com>
2015-06-30 00:46:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+	
 	$('#more-bar-button').click(function(){
 		$('#more-bar').slideToggle();
+	});
+	
+	$('#hidden-nav-bar').click(function(){
+  		if ($('#lower-nav-hidden').css('visibility') == 'hidden')
+    			$('#lower-nav-hidden').css('visibility','visible');
+  		else
+    			$('#lower-nav-hidden').css('visibility','hidden');
 	});
 });
